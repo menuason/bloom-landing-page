ac286f62fe0829c0310f5fbdd5b64799832bc9af
helena <elenamkoian@gmail.com>
2024-02-21 12:43:15 +0400

    feat: add i18n for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,107 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import logo from '../assets/logo/logo-dark.svg';
+import arrowDown from '../assets/icons/arrowDown/arrowDown.svg';
 
-const NAV_BAR_ITEMS = [
+const LANG_ITEMS = [
   {
-    title: 'Catalogue',
-    href: '',
+    lang: 'English',
+    name: 'en',
   },
   {
-    title: 'Packaging',
-    href: '',
+    lang: 'Русский',
+    name: 'ru',
   },
   {
-    title: 'About Us',
-    href: '',
-  },
-  {
-    title: 'Contact',
-    href: '',
-  },
-  {
-    title: 'Eng',
-    href: '',
+    lang: 'հայերեն',
+    name: 'hy',
   },
 ];
 
+const MultiLanguageSelect = () => {
+  const [selectedValue, setSelectedLanguage] = useState('en');
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (language: string) => {
+    setSelectedLanguage(language);
+    i18n.changeLanguage(language);
+  };
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger
+        className="
+          flex items-center cursor-pointer text-black border-none
+          hover:border-none hover:outline-none
+          focus:outline-none
+        "
+      >
+        <div className="flex gap-2 hover:text-green-600">
+          {selectedValue === 'en' ? 'Eng' : selectedValue === 'ru' ? 'Рус' : 'Հայ'}
+          <img src={arrowDown} alt="Arrow Down" />
+        </div>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content
+        align="start"
+        className="
+            absolute text-black bg-white
+            hover:outline-none py-2 px-4"
+      >
+        {
+          LANG_ITEMS.map((item) => (
+            <DropdownMenu.Item
+              onSelect={() => changeLanguage(`${item.name}`)}
+              className="
+                flex self-start px-4 py-2 my-0.5 cursor-pointer
+                hover:outline-none
+              "
+            >
+              {item.lang}
+            </DropdownMenu.Item>
+          ))
+        }
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+};
+
 const Header = () => {
+  const { t } = useTranslation();
+
+  const NAV_BAR_ITEMS = [
+    {
+      title: t('header.home'),
+      href: '',
+    },
+    {
+      title: t('header.catalogue'),
+      href: '',
+    },
+    {
+      title: t('header.packaging'),
+      href: '',
+    },
+    {
+      title: t('header.about.aboutUs'),
+      href: '',
+    },
+    {
+      title: t('header.contact'),
+      href: '',
+    },
+  ];
+
   return (
     <div className="bg-white h-20 flex justify-between px-20">
       <img src={logo} className="self-center h-14 w-[105px] cursor-pointer" alt="logo" />
-      <div className="flex gap-12 items-center cursor-pointer text-black">
-        {NAV_BAR_ITEMS.map((item, ind) => (
-          <a href={item.href} className="hover:text-green-600" key={ind}>{item.title}</a>
-        ))}
+      <div className="flex gap-12">
+        <div className="flex gap-12 items-center cursor-pointer text-black">
+          {NAV_BAR_ITEMS.map((item, ind) => (
+            <a href={item.href} className="hover:text-green-600" key={ind}>{t(`${item.title}`)}</a>
+          ))}
+        </div>
+        <MultiLanguageSelect />
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import { I18nextProvider } from 'react-i18next';
+import i18n from './locale/locale';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
 );
