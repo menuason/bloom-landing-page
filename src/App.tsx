d812df95322a930115e1c51d7fb4841b7f8ad9a3
menuason <menuason@gmail.com>
2024-02-23 13:25:06 +0400

    add: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage/HomePage";
 import PackagingPage from "./pages/PackagingPage/PackagingPage";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <Router>
         <Routes>
-          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/" element={<HomePage />} />
           <Route path="/packaging" element={<PackagingPage />} />
         </Routes>
       </Router>
