97f3fc453f9f4e6b0f8947dbb12c071dd8fa23a4
menuason <menuason@gmail.com>
2024-02-23 14:58:05 +0400

    feat: react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
-import PackagingPage from './pages/PackagingPage/PackagingPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PackagingPage from "./pages/PackagingPage/PackagingPage";
 
 function App() {
   return (
     <>
       <Header />
-        <PackagingPage />
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/packaging" element={<PackagingPage />} />
+        </Routes>
+      </Router>
       <Footer />
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,37 @@
-import logo from '../assets/logo/logo-dark.svg';
-import { useTranslation } from 'react-i18next';
-import MultiLanguageSelect from './MultilanguageSelect';
-import HamburgerSelect from './HamburgerSelect';
+import logo from "../assets/logo/logo-dark.svg";
+import { useTranslation } from "react-i18next";
+import MultiLanguageSelect from "./MultilanguageSelect";
+import HamburgerSelect from "./HamburgerSelect";
 
 const Header = () => {
   const { t } = useTranslation();
 
   const NAV_BAR_ITEMS_WEB = [
     {
-      title: t('header.home'),
-      href: '',
+      title: t("header.home"),
+      href: "/",
     },
     {
-      title: t('header.catalogue'),
-      href: '',
+      title: t("header.catalogue"),
+      href: "",
     },
     {
-      title: t('header.packaging'),
-      href: '',
+      title: t("header.packaging"),
+      href: "/packaging",
     },
     {
-      title: t('header.about.aboutUs'),
-      href: '',
+      title: t("header.about.aboutUs"),
+      href: "",
     },
     {
-      title: t('header.contact'),
-      href: '',
+      title: t("header.contact"),
+      href: "",
     },
   ];
 
   return (
-    <div className="
+    <div
+      className="
       bg-white h-20 w-full flex justify-between px-6
       lg:px-20
       md:px-8
@@ -47,11 +48,15 @@ const Header = () => {
 
       <div className="flex items-center gap-12 cursor-pointer text-black">
         <div className="hidden lg:flex gap-12 items-center">
-          {
-            NAV_BAR_ITEMS_WEB.map((item) => (
-              <a key={item.title} href={item.href} className="hover:text-green-600">{item.title}</a>
-            ))
-          }
+          {NAV_BAR_ITEMS_WEB.map((item) => (
+            <a
+              key={item.title}
+              href={item.href}
+              className="hover:text-green-600"
+            >
+              {item.title}
+            </a>
+          ))}
         </div>
         <div className="flex gap-3">
           <MultiLanguageSelect />
