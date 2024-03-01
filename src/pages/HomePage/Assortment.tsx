ceaa3a4a10a4e8155efeed43e3c8a2b74908caf5
menuason <menuason@gmail.com>
2024-03-01 14:02:42 +0400

    feat: scroll Assortiment images

--- src/pages/HomePage/Assortment.tsx
+++ src/pages/HomePage/Assortment.tsx
@@ -39,7 +39,7 @@ export const Assortment = () => {
 
   return (
     <div className="flex flex-col items-center lg:gap-1 lg:px-20 md:px-8 md:my-6 xs:pl-4 xs:gap-4">
-      <div className="flex flex-col justify-center lg:items-center lg:gap-2 md:gap-1 xs:gap-2 xs:items-start ">
+      <div className="flex flex-col justify-center items-center lg:gap-2 md:gap-1 xs:gap-2">
         <h1
           className="text-gray-950
             lg:font-normal lg:text-5xl
@@ -53,17 +53,26 @@ export const Assortment = () => {
         <h6
           className="text-gray-950 lg:w-10/12 font-light
             lg:text-base lg:text-center
-            md:text-sm
+            md:text-sm  md:text-center
             xs:text-sm xs:items-start
           "
         >
           {t("homePage.assortment.assortmentSectionDescription")}
         </h6>
       </div>
-      <div className="flex flex-row gap-4 mt-9 ">
+      <div
+        className="flex flex-row gap-4 mt-9
+            lg:h-96 lg:overflow-hidden
+            md:h-52 md:overflow-hidden
+            xs:h-52 xs:overflow-x-auto xs:whitespace-nowrap
+          "
+      >
         {flowersArr.map((item, index) => {
           return (
-            <div key={index} className="relative w-full h-full">
+            <div
+              key={index}
+              className="lg:w-1/4 relative h-full md:w-1/4 md:felx-1 xs:w-1/2 xs:shrink-0"
+            >
               <img
                 src={item.img}
                 className="w-full h-full object-cover transition-opacity opacity-100 hover:opacity-0 absolute inset-0 cursor-pointer"
@@ -73,8 +82,10 @@ export const Assortment = () => {
                 alt={`Image ${item.name}`}
                 className="w-full h-full object-cover cursor-pointer"
               />
-              <div className="absolute bottom-0 left-0 px-4 bg-[#23242759] text-white flex items-start justify-center flex-col h-1/5 opacity-1 w-full">
-                <h4 className="text-lg font-medium">{item.name}</h4>
+              <div className="w-full lg:h-1/5 bg-[#23242759] text-white flex items-start justify-center flex-col px-4 absolute bottom-0 left-0 opacity-1 md:h-1/4 xs:h-1/4">
+                <h4 className="font-medium lg:text-2xl md:text-base xs:text-base">
+                  {item.name}
+                </h4>
                 <p className="font-extralight text-xs">{item.author}</p>
               </div>
             </div>
