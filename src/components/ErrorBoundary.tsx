6c0f13733e45ff9f53e3a6f017d1d41fd966e9a0
helena <elenamkoian@gmail.com>
2024-03-12 20:08:33 +0400

    fix: ErrorBoundary.tsx

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -7,7 +7,7 @@ export const ErrorBoundary = () => {
   const error = useRouteError();
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex inset-0 absolute w-fit h-fit m-auto p-2 text-center">
       <div className="flex flex-col gap-6">
         <p className="text-bloomBlack lg:text-lg md:text-base xs:text-sm">
           {
